339028928d26d4896dfa46847c421210e8cf1cf5
MichaelKvak <mkvak1987@gmail.com>
2021-09-07 22:43:30 +0300

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,152 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    schoolList: [],
+    editedObj: null,
+    loading: false,
+    error: null,
+  },
+  getters: {
+    getSchoolList: (state) => state.schoolList,
+    getEditedObj: (state) => state.editedObj,
+    isLoading: (state) => state.loading,
+    hasError: (state) => state.error,
   },
   mutations: {
+    deleteSchool(state, schoolId) {
+      state.schoolList = state.schoolList.filter(
+        (item) => item._id !== schoolId
+      );
+    },
+    setEditedObj(state, obj) {
+      state.editedObj = obj;
+    },
+    setSchoolList(state, list) {
+      state.schoolList = list;
+    },
+    setLoading(state, loading) {
+      state.loading = loading;
+    },
+    setError(state, error) {
+      state.error = error;
+    },
   },
   actions: {
+    loadSchool({ commit }) {
+      commit("setLoading", true);
+      commit("setError", null);
+      axios
+        .get("http://localhost:3000/school")
+        .then((res) => res.data)
+        .then((resData) => {
+          if (resData.success) commit("setSchoolList", resData.data);
+          else throw new Error("Fetch failed!");
+        })
+        .catch((err) => {
+          commit("setError", err);
+        })
+        .finally(() => commit("setLoading", false));
+    },
+
+    deleteSchoolCart({ commit }, schoolId) {
+      commit("setLoading", true);
+      commit("setError", null);
+
+      axios
+        .delete("http://localhost:3000/school", {
+          schoolId,
+        })
+        .then((res) => res.data)
+        .then((resData) => {
+          if (resData.success) commit("deleteSchool", schoolId);
+          else throw new Error("Fetch failed!");
+        })
+        .catch((err) => {
+          commit("setError", err);
+        })
+        .finally(() => commit("setLoading", false));
+    },
+
+    createSchool(
+      { commit },
+      { schoolSubject, schoolTeacher, schoolClass, schoolTime }
+    ) {
+      commit("setLoading", true);
+      commit("setError", null);
+      return new Promise((resolve, reject) => {
+        axios
+          .post("http://localhost:3000/school/add", {
+            schoolSubject,
+            schoolTeacher,
+            schoolClass,
+            schoolTime,
+          })
+          .then((res) => res.data)
+          .then((resData) => {
+            if (resData.success) resolve(true);
+            else throw new Error("Fetch failed!");
+          })
+          .catch((err) => {
+            commit("setError", err);
+            reject(err);
+          })
+          .finally(() => commit("setLoading", false));
+      });
+    },
+
+    getSchoolById({ commit }, schoolId) {
+      commit("setLoading", true);
+      commit("setError", null);
+      return new Promise((resolve, reject) => {
+        axios
+          .get(`http://localhost:3000/school/${schoolId}`)
+          .then((res) => res.data)
+          .then((resData) => {
+            if (resData.success) {
+              commit("setEditedObj", resData.data);
+              resolve(resData.data);
+            } else throw new Error("Fetch failed!");
+          })
+          .catch((err) => {
+            commit("setError", err);
+            reject(err);
+          })
+          .finally(() => commit("setLoading", false));
+      });
+    },
+
+    updateSchool(
+      { commit, state },
+      { schoolSubject, schoolTeacher, schoolClass, schoolTime }
+    ) {
+      commit("setLoading", true);
+      commit("setError", null);
+      return new Promise((resolve, reject) => {
+        axios
+          .put("http://localhost:3000/school/update", {
+            schoolId: state.editedObj._id,
+            schoolSubject,
+            schoolTeacher,
+            schoolClass,
+            schoolTime,
+          })
+          .then((res) => res.data)
+          .then((resData) => {
+            if (resData.success) resolve(true);
+            else throw new Error("Fetch failed!");
+          })
+          .catch((err) => {
+            commit("setError", err);
+            reject(err);
+          })
+          .finally(() => commit("setLoading", false));
+      });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div v-if="isLoading">loading...</div>
+    <div v-if="hasError">error</div>
+    <div v-if="!isLoading && !hasError">
+      <div v-for="school in getSchoolList" :key="school._id">
+        <span>School subject - {{ school.subject }}</span> <br />
+        <span>School teacher - {{ school.teacher }}</span> <br />
+        <span>School class - {{ school.class }}</span> <br />
+        <span>School timing - {{ school.time }}</span>
+        <div><button @click="onDelete(school._id)">delete</button></div>
+        <button @click="onEdit(school._id)">edit</button>
+        <hr />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { mapGetters, mapActions } from "vuex";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  computed: {
+    ...mapGetters(["getSchoolList", "isLoading", "hasError"]),
+  },
+  methods: {
+    ...mapActions(["loadSchool", "deleteSchoolCart"]),
+    onDelete(id) {
+      this.deleteSchoolCart(id);
+    },
+    onEdit(id) {
+      this.$router.push({ name: "About", params: { id } });
+    },
+  },
+  mounted() {
+    this.loadSchool();
+  },
+};
 </script>
